2a84da72c7149b473ee08350ea60c6cdc11ac7a1
Banker <142699168+banker0404@users.noreply.github.com>
2024-04-24 00:12:02 +0530

    Update ExperienceData.ts

--- src/data/ExperienceData.ts
+++ src/data/ExperienceData.ts
@@ -1,6 +1,6 @@
 export const ExperienceData = [
     {
-        ExperienceOrganization: "Randomize Club of Computer Science",
+        ExperienceOrganization: "Randomize CS Club",
         ExperienceWorkTitle: "Working Team Member ",
         ExperienceTimeFrom: "September 2022",
         ExperienceTimeTo: "May 2023",
