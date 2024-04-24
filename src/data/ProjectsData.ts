d1f88ce11c0ba67d0c79ecb52159461cd83d61af
Banker <142699168+banker0404@users.noreply.github.com>
2024-04-24 13:10:37 +0530

    Update ProjectsData.ts

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -19,7 +19,7 @@ export const ProjectsData = [
             "Python", "ML", "Python Libraries",
         ],
         ProjectDescription: [
-            "Built a machine learning model to recommend music based on the current song the user is listening to bases on the genre of the song and the varios numerical attributes of the song like valence, acousticness, danceability, etc.",
+            "Built a machine learning model to recommend music based on the current song the user is listening to with relevance to the genre of the song and the varios numerical attributes of the song like valence, acousticness, danceability, etc.",
             "Used python to implement KNN and K-Means algorithm to make the recommendation system. Used relevant python libraries like pandas, numpy, seaborn, matplotlib, etc.",
         ],
     }
