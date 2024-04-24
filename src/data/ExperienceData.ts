82efc436a0fd4b651ad41f7ed6df4be15868bce1
Banker <142699168+banker0404@users.noreply.github.com>
2024-04-24 19:39:42 +0530

    Update ExperienceData.ts

--- src/data/ExperienceData.ts
+++ src/data/ExperienceData.ts
@@ -2,9 +2,9 @@ export const ExperienceData = [
     {
         ExperienceOrganization: "Randomize CS Club",
         ExperienceWorkTitle: "Working Team",
-        ExperienceTimeFrom: "September 2022",
+        ExperienceTimeFrom: "Sep 2022",
         ExperienceTimeTo: "May 2023",
-        ExperienceLocation: "Jaipur, Rajasthan",
+        ExperienceLocation: "Jaipur",
         ExperienceDescription: [
             "Worked with a team of 75+ members where we hosted various technical events like Hackathons, Datathons and Ideathons.",
             "I was in charge of coming up with ideas for the events. I was also responsible for administrative necessities like permissions and permits required to host the events.",
@@ -13,9 +13,9 @@ export const ExperienceData = [
     {
         ExperienceOrganization: "Glitch!",
         ExperienceWorkTitle: "Organising Committee",
-        ExperienceTimeFrom: "August 2023",
-        ExperienceTimeTo: "April 2024",
-        ExperienceLocation: "Jaipur, Rajasthan",
+        ExperienceTimeFrom: "Aug 2023",
+        ExperienceTimeTo: "Apr 2024",
+        ExperienceLocation: "Jaipur",
         ExperienceDescription: [
             "I worked with a team of 50+ members where we hosted various cultural events related to anime gaming and VFX. We conducted gaming competitions and had workshops where we taught beginners the basic of VFX. We also sent our own team to other colleges to take part in competitions there.",
             "I was responsible for the marketing and promotions of the events and registrations of participants. I along with a team of 5-6 members was responsible for smooth execution of events.",
